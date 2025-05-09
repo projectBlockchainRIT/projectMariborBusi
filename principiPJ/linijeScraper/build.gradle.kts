0253c65f4eb1f7f6dbda3d312e9487f0f1dd7bfa
Adrian <cvetkoadrian@gmail.com>
2025-05-09 07:57:34 +0200

    SC-9 #comment added routes scraping

--- principiPJ/linijeScraper/build.gradle.kts
+++ principiPJ/linijeScraper/build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("it.skrape:skrapeit:1.2.2")
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
 }
 
 tasks.test {
