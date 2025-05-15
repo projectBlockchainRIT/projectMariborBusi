9d61fb4ae1efd6a14ab2c1245a9a95711aad43a4
blaz <blaz1kolman@gmail.com>
2025-05-15 17:02:02 +0200

    SC-44 v projekt dodan webscraper za MarProm in webscraper za splošne podatke postaj

--- principiPJ/DataManagerApp/build.gradle.kts
+++ principiPJ/DataManagerApp/build.gradle.kts
@@ -21,6 +21,16 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("it.skrape:skrapeit:1.2.2")
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 compose.desktop {
