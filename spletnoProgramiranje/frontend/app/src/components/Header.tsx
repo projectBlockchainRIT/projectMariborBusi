33300c693454009ad9d19aebb94a9bcbe1d1dcb6
timi <timi.maucec@gmail.com>
2025-05-28 10:38:01 +0200

    SC-64 cleanal code

--- spletnoProgramiranje/frontend/app/src/components/Header.tsx
+++ spletnoProgramiranje/frontend/app/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Bus, Menu, X } from 'lucide-react';
 
 const Header = () => {
