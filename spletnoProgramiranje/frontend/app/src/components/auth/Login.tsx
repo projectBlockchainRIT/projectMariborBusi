c6e98399db9b1cf6ad338b70ebf387764e5c8782
timi <timi.maucec@gmail.com>
2025-05-31 11:33:49 +0200

    SC-67 dashboard in dodane nekatere animacije...

--- spletnoProgramiranje/frontend/app/src/components/auth/Login.tsx
+++ spletnoProgramiranje/frontend/app/src/components/auth/Login.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Mail, Lock, ArrowRight } from 'lucide-react';
+import { Bus } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -11,6 +13,15 @@ const Login = () => {
   };
 
   return (
+    <>
+    <div className="container mx-auto px-4 py-4 sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between">
+        <Link to="/" className="flex items-center">
+        <Bus className="h-8 w-8 text-mbusi-red-600" />
+        <span className="ml-2 text-2xl font-bold text-mbusi-red-600">M-busi</span>
+        </Link>
+      </div>
+      </div>
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
@@ -104,6 +115,7 @@ const Login = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
