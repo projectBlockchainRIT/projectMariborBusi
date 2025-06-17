de57819a297fabb64119a92b809b8976b9b9ebf7
Adrian <cvetkoadrian@gmail.com>
2025-06-17 18:44:21 +0200

    SC-50 deleted unnecessary files

--- spletnoProgramiranje/dashboard/src/components/Map.tsx
+++
@@ -1,9 +0,0 @@
-import RealTimeMapBox from './RealTimeMapBox';
-
-export default function Map() {
-  return (
-    <div className="w-full h-full bg-white dark:bg-gray-800 rounded-lg shadow-lg">
-      <RealTimeMapBox />
-    </div>
-  );
-} 
