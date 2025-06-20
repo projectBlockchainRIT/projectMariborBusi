fc44c7a5c1a55979a149adc01d9fb34c97fa339a
Adrian <cvetkoadrian@gmail.com>
2025-06-20 09:12:36 +0200

    ci/cd demonstracija

--- spletnoProgramiranje/backend/cmd/api/main.go
+++ spletnoProgramiranje/backend/cmd/api/main.go
@@ -48,7 +48,7 @@ func main() {
 
 	defer db.Close()
 	// logger.Info("established database connection")
-	// logger.Info("Testiram za webhook 3")
+	logger.Info("CI/CD demonstracija")
 
 	store := data.NewStorage(db)
 
